513263dffc0ecd7bcbc8ce16dcd64d11d78592d5
Lars Kaare Skjorestad <lars.kaare@skjorestad.com>
2019-10-01 10:01:44 +0200

    Adding initial client for ex-8

---
+++ client/server.js
@@ -0,0 +1,27 @@
+/*
+*  Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license.
+*  See LICENSE in the source repository root for complete license information.
+*/
+
+var express = require('express');
+var app = express();
+var morgan = require('morgan');
+var path = require('path');
+
+// Initialize variables.
+var port = 30662; // process.env.PORT || 30662;
+
+// Configure morgan module to log all requests.
+app.use(morgan('dev'));
+
+// Set the front-end folder to serve public assets.
+app.use(express.static('JavaScriptSPA'))
+
+// Set up our one route to the index.html file.
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+// Start the server.
+app.listen(port);
+console.log('Listening on port ' + port + '...');
