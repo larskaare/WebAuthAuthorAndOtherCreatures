77c5ac75d1f74f41640f8f45fd566029ed867a8c
Lars Kaare Skjorestad <lars.kaare@skjorestad.com>
2019-10-07 14:25:01 +0200

    Adding code to example 9 - protect api

---
+++ ex-9/api/app.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var cors = require('cors');
+
+var indexRouter = require('./routes/index');
+
+var app = express();
+
+var corsOptions = {
+    origin: 'http://localhost',
+    optionsSuccessStatus: 200 
+  }
+
+app.use(cors(corsOptions));
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use('/', indexRouter);
+
+module.exports = app;
