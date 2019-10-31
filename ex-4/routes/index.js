2671fd7681b62b717c2ee4b25f5b4ee0ec073ffe
Lars Kaare Skjorestad <lars.kaare@skjorestad.com>
2019-10-31 11:57:49 +0100

    Preparing for ex-4, getting an access token

--- ex-4/routes/index.js
+++ ex-4/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-    res.render('index', { title: 'My Authentication, Authorization and MS Graph demo app (EX-3)' });
+    res.render('index', { title: 'My Authentication, Authorization and MS Graph demo app (EX-4)' });
 });
 
 module.exports = router;
